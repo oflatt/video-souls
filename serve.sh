68a8c84e380bbf5a1ed30621b3909f9c70cd8f3d
oflatt <oflatt@gmail.com>
2024-12-20 18:28:11 -0700

    serve for python3

--- serve.sh
+++ serve.sh
@@ -1,8 +1,16 @@
 tsc --watch &
 PIDS[0]=$!
 
+echo "Starting server on http://localhost:8001"
 
-python -m http.server 8001 &
+# if python3 exists use that
+if command -v python3 &> /dev/null
+then
+    python3 -m http.server 8001 &
+else
+    python -m http.server 8001 &
+fi
+ 
 PIDS[1]=$!
 
 trap "kill ${PIDS[*]}" SIGINT
