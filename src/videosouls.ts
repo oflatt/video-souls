0da93ab05901504a3f953f174bdfa5a844fa966e
oflatt <oflatt@gmail.com>
2025-07-05 21:29:48 -0700

    break out battle.rs

--- src/videosouls.ts
+++ src/videosouls.ts
@@ -4,6 +4,7 @@ import { Editor, levelDataFromVideo, LevelDataV0, validateLevelData, BossState, 
 import { Graphics } from './graphics';
 import { InputManager, InputDirection } from './inputmanager';
 import { AudioPlayer } from './audioPlayer';
+import { BattleRenderer, AttackAnimation, BattleState, initialBattleState, directionNumToSwordPos, directionNumToSwordAngle } from './battle';
 
 // Load the interpreter from the local acorn_interpreter.js file
 declare const Interpreter: any;
@@ -12,14 +13,6 @@ enum GameMode {
   MENU, PLAYING, BATTLE_END, EDITING
 }
 
-enum AttackAnimation {
-  NONE, PARRYING, ATTACK_STARTING, ATTACKING, STAGGERING
-}
-
-const PARRY_WINDOW = 0.2;
-const PARRY_END_LAG = 0.2;
-const SUCCESS_PARRY_ANIM_FADE = 0.2;
-
 const ATTACK_COMBO_STARTUP_TIMES = [0.2, 0.2, 0.3, 0.2, 0.4];
 const ATTACK_COMBO_DAMAGE_MULT = [1.0, 1.1, 1.3, 1.0, 2.2];
 const ATTACK_END_LAG = 0.15;
@@ -27,71 +20,14 @@ const COMBO_EXTEND_TIME = 3.0; // number of seconds before combo lapses
 
 const STAGGER_TIME = 0.4;
 
-const ATTACK_WARNING_ADVANCE = 0.5;
-
-type BattleState = {
-  anim: {
-    state: AttackAnimation,
-    startTime: number,
-    endTime: number,
-    startPos: [number, number],
-    endPos: [number, number],
-    // directions in radians on the unit circle, blade of the sword points in this direction
-    startAngle: number,
-    endAngle: number,
-    // last parry time adds a green glow after a successful parry
-    lastParryTime: number,
-    startYScale: number,
-    endYScale: number,
-    startXScale: number,
-    endXScale: number,
-  },
-  hitCombo: number, // number of successful 
-  hitComboTime: number, // last time the combo was extended
-  // inputs are buffered so that they are not missed and punishes for spam
-  bufferedInput: string | null,
-  playerHealth: number,
-  lastPlayerHealth: number,
-  lastPlayerHit: number, // the time the player was last hit
-  bossHealth: number,
-  lastBossHealth: number,
-  lastBossHit: number, // the time the boss was last hit
-  // the last time we checked for attacks
-  prevTime: number,
-  // Boss AI state
-  currentInterval: string,
-};
+const PARRY_WINDOW = 0.2;
+const PARRY_END_LAG = 0.2;
 
 type AlertData = {
   message: HTMLElement,
   startTime: number,
   lifetime: number
 };
-
-const directionNumToSwordAngle = new Map<number, number>([
-  [0, 4 * Math.PI / 4],
-  [1, 3 * Math.PI / 4],
-  [2, 2 * Math.PI / 4],
-  [3, 1 * Math.PI / 4],
-  [4, 4 * Math.PI / 4],
-  [5, 3 * Math.PI / 4],
-  [6, 2 * Math.PI / 4],
-  [7, 5 * Math.PI / 4],
-  [8, 2 * Math.PI / 4],
-]);
-
-// positions relative to center of screen
-const directionNumToSwordPos = new Map<number, [number, number]>([
-  [0, [0.0, 0.2]],
-  [1, [0.2, 0.2]],
-  [2, [0.2, 0.0]],
-  [3, [0.2, -0.2]],
-  [4, [0.0, -0.2]],
-  [5, [-0.2, -0.2]],
-  [6, [-0.2, 0.0]],
-  [7, [-0.2, 0.2]],
-  [8, [0.0, 0.0]],
-]);
 
 // position relative to bottom left of screen
 const attackedPosition = [0.7, 0.4];
@@ -105,6 +41,7 @@ export class VideoSouls {
   graphics: Graphics;
   audio: AudioPlayer;
   inputManager: InputManager;
+  battleRenderer: BattleRenderer;
   // only defined when in editing mode
   editor: Editor;
 
@@ -140,6 +77,7 @@ export class VideoSouls {
     } as const;
 
     this.graphics = new Graphics(this.elements.canvas);
+    this.battleRenderer = new BattleRenderer(this.graphics, this.elements.canvas);
     this.editor = new Editor(player, this.elements.recordingControls, this.elements.playbackBar, new LevelDataV0(), this.graphics);
     this.gameMode = GameMode.MENU;
     this.battle = initialBattleState();
@@ -616,6 +554,10 @@ export class VideoSouls {
       // clone the target position so we don't mutate it!
       var targetPos = [...directionNumToSwordPos.get(this.inputManager.getCurrentTargetDirection())!];
   
+      // offset the target position to center of screen
+      targetPos[0] += 0.5;
+      targetPos[1] += 0.5;
+  
       // if the position is some epsilon close to the target position, set the position to the target position
       if (Math.abs(this.battle.anim.endPos[0] - targetPos[0]) < 0.01 && Math.abs(this.battle.anim.endPos[1] - targetPos[1]) < 0.01) {
         this.battle.anim.endPos[0] = targetPos[0];
@@ -765,140 +707,33 @@ export class VideoSouls {
     }
   }
 
-  private drawAttackWarning() {
+  private drawCanvas() {
     const currentTime = this.elements.player.getCurrentTime();
-    const ctx = this.elements.canvas.getContext('2d')!;
-  
-    // check for attack warning sound
-    const soundAttack = this.getAttacksInInterval(this.battle.prevTime + ATTACK_WARNING_ADVANCE, currentTime + ATTACK_WARNING_ADVANCE);
-  
-    if (soundAttack.length > 0) {
-      this.audio.playWarningSound();
-    }
-  
-    const animAttacks = this.getAttacksInInterval(currentTime, currentTime + ATTACK_WARNING_ADVANCE);
-    for (const attack of animAttacks) {
-      const attackPos = [...directionNumToSwordPos.get(attack.direction)!];
-      // make attack pos a bit futher from the center
-      attackPos[0] = attackPos[0] * 1.5;
-      attackPos[1] = attackPos[1] * 1.5;
-  
-      // offset the attack position to center of screen
-      attackPos[0] += 0.5;
-      attackPos[1] += 0.5;
-  
-      const animTime = (currentTime - attack.time) / ATTACK_WARNING_ADVANCE;
-      const opacity = Math.max(0, 1 - animTime);
-  
-      const attackX = this.elements.canvas.width * attackPos[0];
-      const attackY = this.elements.canvas.height * (1 - attackPos[1]);
-      ctx.save();
-      ctx.globalAlpha = opacity;
-
-      // draw the arrow sprite
-      ctx.translate(attackX, attackY);
-      // rotate by the angle of the attack
-      ctx.rotate(attack.direction * Math.PI / 4);
-      ctx.drawImage(this.graphics.arrowSprite, -this.graphics.arrowSprite.width / 2, -this.graphics.arrowSprite.height / 2);
-
-      ctx.restore();
-    }
+    const youtubeVideoName = this.elements.player.getIframe().title;
+    
+    this.battleRenderer.drawCanvas(
+      currentTime,
+      this.battle,
+      this.getAttacksInInterval.bind(this),
+      this.audio.playWarningSound.bind(this.audio),
+      this.inputManager.getCurrentTargetDirection.bind(this.inputManager),
+      youtubeVideoName
+    );
   }
 
   private drawSword() {
     const currentTime = this.elements.player.getCurrentTime();
-    var swordPos = this.battle.anim.endPos;
-    var swordAngle = this.battle.anim.endAngle;
-    var redSwordOutlineStrength = 0.0;
-    var greenSwordOutlineStrength = 0.0;
-    var xscale = 1.0;
-    var yscale = 1.0;
-  
-    // first, determine swordPos and swordDir from animation
-    if (this.battle.anim.state !== AttackAnimation.NONE) {
-      const animProgressUncapped = (currentTime - this.battle.anim.startTime) / (this.battle.anim.endTime - this.battle.anim.startTime);
-      const animProgress = Math.max(Math.min(1.0, animProgressUncapped), 0.0);
-      swordPos = [
-        this.battle.anim.startPos[0] + (this.battle.anim.endPos[0] - this.battle.anim.startPos[0]) * animProgress,
-        this.battle.anim.startPos[1] + (this.battle.anim.endPos[1] - this.battle.anim.startPos[1]) * animProgress,
-      ];
-  
-      const fastExponentialAnimProgress = Math.sqrt(Math.sqrt(animProgress));
-      const slowExponentialAnimProgress = Math.pow(animProgress, 0.8);
-      var targetAngle = this.battle.anim.endAngle;
-      // if target angle is more than 180 degrees away, subtract 360 degrees so we rotate the other way
-      if (targetAngle - this.battle.anim.startAngle > Math.PI) {
-        targetAngle -= Math.PI * 2;
-      }
-      // if the target angle is less than -180 degrees away, add 360 degrees so we rotate the other way
-      if (targetAngle - this.battle.anim.startAngle < -Math.PI) {
-        targetAngle += Math.PI * 2;
-      }
-      var currentAngle = this.battle.anim.startAngle + (targetAngle - this.battle.anim.startAngle) * fastExponentialAnimProgress;
-      swordAngle = currentAngle;
-  
-      // sword outline is only visible during the parry window
-      const parryWindowProportion = PARRY_WINDOW / (PARRY_WINDOW + PARRY_END_LAG);
-      if (this.battle.anim.state === AttackAnimation.PARRYING && animProgress < parryWindowProportion) {
-        redSwordOutlineStrength = Math.sqrt(1.0 - (animProgress / parryWindowProportion));
-      }
-
-      // interpolate squish using slowExponentialAnimProgress
-      xscale = this.battle.anim.startYScale + (this.battle.anim.endXScale - this.battle.anim.startXScale) * slowExponentialAnimProgress;
-      yscale = this.battle.anim.startYScale + (this.battle.anim.endYScale - this.battle.anim.startYScale) * slowExponentialAnimProgress;
-    }
-
-    // fading green outline if we just parried successfully
-    if (currentTime - this.battle.anim.lastParryTime < SUCCESS_PARRY_ANIM_FADE) {
-      greenSwordOutlineStrength = Math.sqrt(1.0 - ((currentTime - this.battle.anim.lastParryTime) / SUCCESS_PARRY_ANIM_FADE));
-    }
-
-    // draw swordImage to the canvas at it's current position
-    // center it on the xpos and ypos
-    const topLeftX = this.elements.canvas.width * swordPos[0];
-    const topLeftY = this.elements.canvas.height * swordPos[1];
-    var swordOutlineX = topLeftX;
-    var swordOutlineY = topLeftY; 
-  
-    this.drawCenteredRotated(this.graphics.swordSprites.yellowOutline, swordOutlineX, swordOutlineY, swordAngle - Math.PI / 2, redSwordOutlineStrength, xscale, yscale);
-    this.drawCenteredRotated(this.graphics.swordSprites.greenOutline, swordOutlineX, swordOutlineY, swordAngle- Math.PI / 2, greenSwordOutlineStrength, xscale, yscale);
-    this.drawCenteredRotated(this.graphics.swordSprites.default, topLeftX, topLeftY, swordAngle- Math.PI / 2, 1.0, xscale, yscale);
-  }
-
-  // Draw the sword, health bars, ect to the canvas based on the data in state
-  private drawCanvas() {
-    const currentTime = this.elements.player.getCurrentTime();
-  
-    this.drawAttackWarning();
-  
-    this.drawSword();
-
-    // draw the boss name
-    const youtubeVideoName = this.elements.player.getIframe().title;
-    animateBossName(youtubeVideoName, this.elements.canvas, currentTime, 0.15);
-
-    // draw the boss health at the top
-    drawHealthBar(this.elements.canvas, 0.05, { r: 255, g: 0, b: 0 }, this.battle.bossHealth, this.battle.lastBossHit, this.battle.lastBossHealth, currentTime);
-    // draw the player health at the bottom
-    drawHealthBar(this.elements.canvas, 0.9, { r: 0, g: 255, b: 0 }, this.battle.playerHealth, this.battle.lastPlayerHit, this.battle.lastPlayerHealth, currentTime);
-  }
-  
-  // rotates by angle counter clockwise, and squishes by squish
-  private drawCenteredRotated(image: HTMLImageElement | HTMLCanvasElement, xpos: number, ypos: number, angle: number, alpha: number, xscale: number, yscale: number) {
-    // invert ypos since it starts from the bottom of the screen
-    ypos = this.elements.canvas.height - ypos;
-    const ctx = this.elements.canvas.getContext('2d')!;
-    ctx.save();
-    ctx.globalAlpha = alpha;
-    ctx.translate(xpos, ypos);
-    ctx.rotate(-angle);
-    ctx.scale(xscale, yscale);
-    ctx.drawImage(image, -image.width / 2, -image.height / 2);
-    ctx.restore();
+    this.battleRenderer.drawSword(
+      currentTime, 
+      this.battle, 
+      this.inputManager.getCurrentTargetDirection.bind(this.inputManager)
+    );
   }
 
   currentTargetAngleRadians() {
-    return directionNumToSwordAngle.get(this.inputManager.getCurrentTargetDirection())!;
+    return this.battleRenderer.getCurrentTargetAngleRadians(
+      this.inputManager.getCurrentTargetDirection.bind(this.inputManager)
+    );
   }
 }
 
@@ -909,217 +744,8 @@ function extractVideoID(url: string) {
   return match ? match[1] : null;
 }
 
-function animateBossName(
-  name: string,
-  canvas: HTMLCanvasElement,
-  timeElapsed: number,
-  yPosition: number // Y position as a fraction of the canvas height (0 = top, 1 = bottom)
-) {
-  const ctx = canvas.getContext('2d');
-  if (!ctx) return;
-  ctx.save();
-
-  const width = canvas.width;
-  const height = canvas.height;
-
-  // Set up text properties with a gothic, dramatic font
-  ctx.font = '50px "Cormorant Unicase", serif';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-
-  // Parameters for the animation
-  const waveAmplitude = 10; // Amplitude of the sine wave under the text
-  const waveFrequency = 1.5; // Fewer oscillations (lower frequency)
-  const maxWaveLengthMultiplier = 1.2; // 20% wider than the text width
-  const animationDuration = 1; // Duration in seconds to animate the wave
-  const flashDuration = 0.5; // Duration of transition to red drop shadow
-  const redTextDuration = 0.5; // Duration the red text stays on screen
-  const fadeOutDuration = 0.5; // Duration of fade-out
-
-  const totalDuration =
-    animationDuration + flashDuration + redTextDuration + fadeOutDuration;
-
-  // Stop rendering after the entire animation
-  if (timeElapsed > totalDuration) return;
-
-  // Calculate the width of the text
-  const textWidth = ctx.measureText(name).width;
-
-  // Calculate maximum wave length based on text width
-  const maxWaveLength = textWidth * maxWaveLengthMultiplier;
-
-  // Adjust wave length with an exponential easing function
-  const t = Math.min(timeElapsed / animationDuration, 1); // Cap at 1
-  const waveLength = maxWaveLength * (1 - Math.pow(2, -10 * t)); // Exponential ease-out
-
-  // Calculate the Y position of the text and wave based on the parameter
-  const textY = height * yPosition;
-
-  // Handle fade-out phase
-  let alpha = 1;
-  if (timeElapsed > animationDuration + flashDuration + redTextDuration) {
-    const fadeOutFactor =
-      (timeElapsed - (animationDuration + flashDuration + redTextDuration)) /
-      fadeOutDuration;
-    alpha = 1 - Math.min(fadeOutFactor, 1); // Gradually reduce alpha
-  }
-
-  // Set global alpha for fade-out
-  ctx.globalAlpha = alpha;
-
-  // Transition to red drop shadow or outline immediately after animation ends
-  if (timeElapsed >= animationDuration) {
-    const transitionFactor = Math.min(
-      (timeElapsed - animationDuration) / flashDuration,
-      1
-    ); // Normalize for flash duration
-    const redIntensity = 255;
-    ctx.fillStyle = `rgba(255, 255, 255, 1)`;
-    ctx.shadowColor = `rgba(${redIntensity}, 0, 0, 1)`;
-    ctx.shadowBlur = 15;
-    ctx.shadowOffsetX = 5 + 4 * transitionFactor; // Gradually increase offset
-    ctx.shadowOffsetY = 5 + 4 * transitionFactor;
-  } else {
-    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-    ctx.shadowColor = 'transparent';
-    ctx.shadowBlur = 0;
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
-  }
-
-  // Draw the text
-  ctx.fillText(name, width / 2, textY);
-
-  // Clear the shadow for the wave
-  ctx.shadowColor = 'transparent';
-
-  // Draw the animated wave expanding from the center, keeping it visible until the fade-out ends
-  ctx.beginPath();
-  const startX = (width - waveLength) / 2;
-  const endX = startX + waveLength;
-
-  // Create a smoother wave by adjusting the X increment
-  const step = Math.max(waveLength / 100, 1); // Adjust step size based on wave length
-  for (let x = startX; x < endX; x += step) {
-    // Calculate the sine wave for each x
-    const sineY =
-      waveAmplitude * Math.sin(((x - startX) / waveLength) * waveFrequency * Math.PI * 2);
-    ctx.lineTo(x, textY + 30 + sineY);
-  }
-
-  // Finalize the line path
-  ctx.lineTo(endX, textY + 30); // End point of the curve
-  ctx.strokeStyle = `rgba(255, 255, 255, 0.7)`;
-  ctx.lineWidth = 2;
-  ctx.stroke();
-
-  // Restore canvas state
-  ctx.restore();
-}
-
-interface Color {
-  r: number;
-  g: number;
-  b: number;
-  a?: number; // Optional alpha value, defaults to 1 if not provided
-}
-
-function colorToString(color: Color): string {
-  const { r, g, b, a = 1 } = color;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
-
-function adjustColorOpacity(color: Color, opacity: number): Color {
-  return { ...color, a: opacity };
-}
-
-function drawHealthBar(
-  canvas: HTMLCanvasElement, 
-  yPosition: number, // value from 0 to 1, relative to canvas height
-  color: Color, 
-  currentHealth: number, // value from 0 to 1
-  lastHealthChangeTime: number, // in seconds
-  lastHealth: number, // value from 0 to 1
-  currentTime: number // in seconds
-) {
-  const ctx = canvas.getContext('2d')!;
-  const barWidth = canvas.width * 0.8;
-  const barHeight = 20;
-  const xOffset = (canvas.width - barWidth) / 2;
-  const shakeDuration = 0.2; // seconds
-  const shakeMagnitude = 10; // pixels (increased for more dramatic effect)
-
-  // Calculate the actual y position based on the canvas height
-  const yPos = yPosition * canvas.height;
-
-  // Determine how much health was lost
-  const lostHealth = lastHealth - currentHealth;
-
-  // Time since the health change
-  const timeSinceChange = currentTime - lastHealthChangeTime;
-
-  // Shake effect for the entire bar after getting hit
-  let shakeOffsetX = 0;
-  if (timeSinceChange < shakeDuration) {
-    shakeOffsetX = Math.sin((timeSinceChange / shakeDuration) * Math.PI) * shakeMagnitude;
-  }
-
-  // Draw lost health bar (darker bar, tinted with the same color)
-  if (lostHealth > 0) {
-    const delay = 0.5; // seconds delay before starting the lost health animation
-    let animatedLastHealth = lastHealth;
-    if (timeSinceChange > delay) {
-      const decrementAmount = (timeSinceChange - delay) / 5; // Slow decrease over time (5 seconds)
-      animatedLastHealth = Math.max(currentHealth, lastHealth - decrementAmount);
-    }
-    const lostHealthWidth = barWidth * animatedLastHealth;
-    ctx.fillStyle = colorToString(adjustColorOpacity(color, 0.5)); // Darker tint of the original color
-    ctx.fillRect(xOffset + shakeOffsetX, yPos, lostHealthWidth, barHeight);
-  }
-
-  // Draw current health bar
-  const currentHealthWidth = barWidth * currentHealth;
-  ctx.fillStyle = colorToString(color);
-  ctx.fillRect(xOffset + shakeOffsetX, yPos, currentHealthWidth, barHeight);
-
-  // Draw border
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-  ctx.strokeRect(xOffset + shakeOffsetX, yPos, barWidth, barHeight);
-}
-
 function normalize(vec: [number, number]) {
   const [x, y] = vec;
   const length = Math.sqrt(x * x + y * y);
   return [x / length, y / length];
-}
-
-function initialBattleState(): BattleState {
-  return {
-    anim: {
-      state: AttackAnimation.NONE,
-      startTime: 0,
-      endTime: 0,
-      startPos: [0.5, 0.5],
-      endPos: [0.5, 0.5],
-      startAngle: 0,
-      endAngle: 0,
-      lastParryTime: -100,
-      startYScale: 1.0,
-      endYScale: 1.0,
-      startXScale: 1.0,
-      endXScale: 1.0,
-    },
-    bufferedInput: null,
-    playerHealth: 1.0,
-    lastPlayerHealth: 1.0,
-    lastPlayerHit: -100000000,
-    bossHealth: 1.0,
-    lastBossHealth: 1.0,
-    lastBossHit: -100000000,
-    prevTime: 0,
-    hitCombo: 0,
-    hitComboTime: -10000000,
-    currentInterval: "intro",
-  };
 }
